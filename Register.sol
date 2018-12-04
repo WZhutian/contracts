e0cea6d9c63687b3d92ecae3bcb1e747258ab07f
wuzhutian <wzhutian@gmail.com>
2018-12-04 11:15:59 +0800

    test

--- Register.sol
+++ Register.sol
@@ -129,9 +129,9 @@ contract Register {
     }
 
     //测试签名
-    function test(address a,address b,string c,string d,bytes32[] sig) constant public returns(address){
+    function test(address a,address b,string c,string d,bytes32[] sig,address addr) constant public returns(bool){
         bytes32 params = keccak256(a,b,c,d);
-        return checkSign(params,sig);
+        return checkSign(params,sig) == addr;
     }
 
     /* 签名验证 */
